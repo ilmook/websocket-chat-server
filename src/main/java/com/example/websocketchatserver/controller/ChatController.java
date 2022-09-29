d2ae93780a4a1b127d5c1730fc69df5f0993128d
SK Planet 강일묵 <ilmook@sk.com>
2022-09-29 13:27:35 +0900

    stomp-websocket-server

--- src/main/java/com/example/websocketchatserver/controller/ChatController.java
+++ src/main/java/com/example/websocketchatserver/controller/ChatController.java
@@ -1,26 +1,20 @@
 package com.example.websocketchatserver.controller;
 
-import com.example.websocketchatserver.model.ChatRoom;
-import com.example.websocketchatserver.service.ChatService;
+import com.example.websocketchatserver.model.ChatMessage;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/chat")
+@Controller
 public class ChatController {
 
-    private final ChatService chatService;
-
-    @PostMapping
-    public ChatRoom createRoom(@RequestParam String name) {
-        return chatService.createRoom(name);
-    }
-
-    @GetMapping
-    public List<ChatRoom> findAllRoom() {
-        return chatService.findAllRoom();
+    private final SimpMessageSendingOperations messagingTemplate;
+    @MessageMapping("/chat/message")
+    public void message(ChatMessage message) {
+        if (ChatMessage.MessageType.ENTER.equals(message.getType()))
+            message.setMessage(message.getSender() + "님이 입장하셨습니다.");
+        messagingTemplate.convertAndSend("/topic/chat/room/" + message.getRoomId(), message);
     }
 }
